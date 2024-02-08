0a419d1dcd5cbf61de266d198ed9f3b448746a5f
SOURAV GUPTA <118287650+SURVI1508@users.noreply.github.com>
2024-02-08 21:46:16 +0530

    test auth route

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Express on Vercel 💥💥");
 });
+
+app.get("/api/auth/signin", (req, res) => {
+  res.json({ message: "Welcome to authentication!" });
+});
+
 const PORT = 3000;
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
